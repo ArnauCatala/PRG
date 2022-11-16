c72ef80b6f616f33c6e78c2ad1b9bee7f6782ccc
Arnau <acatalacervera@gmail.com>
2022-11-16 18:20:52 +0100

    1

---
+++ Tema_5/Entrega_arrays_2.java
@@ -0,0 +1,35 @@
+package Tema_5;
+
+
+
+/**
+ * Escriviu aquí una descripcìó de la classe Entrega_arrays_2
+ * 
+ * @author (el vostre nom) 
+ * @version (un número de versió o la data)
+ */
+public class Entrega_arrays_2 {
+
+    // Variables d'instància. Canvieu l'exemple d'aquí sota pels vostres exemples
+    private int x;
+
+    /**
+     * Mètode constructor per a objectes de la classe Entrega_arrays_2
+     */
+    public Entrega_arrays_2() {
+        // Heu d'inicialitzar les variables d'instància
+        x = 0;
+    }
+
+    /**
+     * Un exemple de mètode. Poseu-hi el vostre comentari
+     * 
+     * @param  y   Un paràmetre de mostra: ha de ser un nombre enter.
+     * @return     La suma de x amb y 
+     */
+    public int metodeDeMostra (int y) {
+        // poseu el vostre codi a continuació:
+        return x + y;
+    }
+    
+}
