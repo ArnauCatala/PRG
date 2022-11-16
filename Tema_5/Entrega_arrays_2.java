2d1986c69537dac05b17d9f0f71d0b42e6000a53
Arnau <acatalacervera@gmail.com>
2022-11-16 20:06:33 +0100

    2

--- Tema_5/Entrega_arrays_2.java
+++ Tema_5/Entrega_arrays_2.java
@@ -78,4 +78,61 @@ public class Entrega_arrays_2 {
         System.out.println("Damunt de la mitja: " + dalt);
         System.out.println("Baix de la mitja: " + baix);
     }
+    public static void Act4(){
+        int array1[] = new int[100];
+        int array2[] = new int[100];
+        for(int i = 0; i<100 ; i++){
+            array1[i]=i+1;
+            System.out.print(array1[i]+ " ");
+        }
+        System.out.println();
+        for(int i = 0; i<100 ; i++){
+            array2[i]=array1[99-i];
+            System.out.print(array2[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void Act5(){
+        int array1[] = new int[10];
+        Scanner s = new Scanner(System.in);
+        int P;
+        String st = " ";
+        char r = ' ';
+        while(r!='c'){
+            System.out.println("a. Mostrar valors." + '\n' + "b. Introduir valors"+ '\n' + "c. Eixir");
+            st = s.next();
+            if(st.length()!=1){
+                System.out.println("Input incorrecte");
+            }
+            else{
+                r = st.charAt(0);
+                switch (r){
+                    case 'a':
+                        for(int i = 0; i<10 ; i++){
+                            System.out.print(array1[i]+ " ");
+                        }
+                        System.out.println();
+                        break;
+                    case 'b':
+                        System.out.println("Introdueix un valor");
+                        int n = s.nextInt();
+                        System.out.println("Introdueix una posicio");
+                        int p = s.nextInt()-1;
+                        if(0>p||p>9){
+                            System.out.println("Fora dels llimits del array");
+                        }
+                        else{
+                            array1[p]=n;
+                            break;
+                        }
+                    case 'c':
+                        System.out.println("Fi del programa");
+                        break;
+                    default:
+                        System.out.println("Input incorrecte");
+                        break;
+                }
+            }
+        }
+    }
 }
